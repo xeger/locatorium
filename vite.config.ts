c87c652da010f14e5b5cb43fd354d68f881f0224
Tony Spataro <tony@crossnokaye.com>
2024-12-29 12:28:59 -0800

    Support multiple vitest providers

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
@@ -20,23 +19,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
-    },
-  },
-  test: {
-    include: ['src/**/*.test.{ts,tsx}'],
-    globals: true,
-    setupFiles: ['./vitest-setup.ts', 'vitest-browser-react'],
-    coverage: {
-      provider: 'istanbul',
-      reporter: ['text', 'json', 'html'],
-      include: ['src/**/*.{ts,tsx}'],
-      exclude: ['src/**/*.test.{ts,tsx}'],
-    },
-    browser: {
-      enabled: true,
-      headless: true,
-      instances: [{ browser: 'chromium' }],
-      provider: 'playwright',
     },
   },
 });
---
+++ vitest-webdriverio.config.ts
@@ -0,0 +1,12 @@
+import { defineProject, mergeConfig } from 'vitest/config'
+import configShared from './vitest.config.ts'
+
+export default defineProject(mergeConfig(configShared, {
+  test: {
+    browser: {
+      headless: true,
+      instances: [{ browser: 'chrome' }],
+      provider: 'webdriverio',
+    },
+  },
+}))
