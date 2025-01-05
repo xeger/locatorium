b1d2f5e30b57ae6588b08d7b18cf827db5d75fbf
Tony Spataro <tony@crossnokaye.com>
2025-01-05 11:47:20 -0800

    Document broken right click better

--- src/routes/interactivity.test.tsx
+++ src/routes/interactivity.test.tsx
@@ -13,6 +13,7 @@ describe('via Locator', () => {
     const target = page.getByTestId('right-click-target');
     await expect.element(target).toBeInTheDocument();
     await expect.element(page.getByRole('menu')).not.toBeInTheDocument();
+    // BUG: passes w/ Playwright; but unexpectedly fails w/ WebdriverIO (and it's impossible to right click in w/ Preview?!)
     await target.click({ button: 'right' });
     await expect.element(page.getByRole('menu')).toBeInTheDocument();
   })
@@ -20,10 +21,15 @@ describe('via Locator', () => {
 
 // In Vitest, the userEvent singleton is the documented interactivity API.
 describe('via userEvent', () => {
+  // Right click is a [provider-dependent operation](https://vitest.dev/guide/browser/interactivity-api.html#userevent-click) in Vitest.
+  //   - [WebdriverIO](https://webdriver.io/docs/api/element/click/) claims to support `button` option
+  //   - [Playwright](https://playwright.dev/docs/api/class-locator#locator-click) claims to support `button` option
+  //   - [Testing Library](https://testing-library.com/docs/user-event/convenience/#click) has a completely different way of doing right click
   test('right click', async () => {
     const target = page.getByTestId('right-click-target');
     await expect.element(target).toBeInTheDocument();
     await expect.element(page.getByRole('menu')).not.toBeInTheDocument();
+    // BUG: passes w/ Playwright; but unexpectedly fails w/ WebdriverIO (and it's impossible to right click in w/ Preview?!)
     await userEvent.click(target, { button: 'right' });
     await expect.element(page.getByRole('menu')).toBeInTheDocument();
   })
